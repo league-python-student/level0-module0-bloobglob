b9044fc19afea7d626abbacd379659627e45a9ed
root <root@0715bc7e7dd8>
2020-08-03 21:56:16 +0000

    Day 1

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -5,32 +5,48 @@ if __name__ == '__main__':
     window.bgcolor('white')
     
     # This code makes a new Turtle. Pick a new name for the turtle
-    myTurtle = turtle.Turtle()
+    lucas = turtle.Turtle()
     
     # Make your turtle's shape 'turtle', .shape('turtle')
-    
+    lucas.shape('turtle')
     # Set your turtle's speed using .speed(2)
-    
+    lucas.speed(2)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-    
+    lucas.color('green')
+    lucas.pencolor('blue')
     # Move your turtle forward using .forward(100)
     # TEST    Did your turtle move forward?
-    
+    lucas.forward(100)
     # Move your turtle left or right using .left(90) or .right(90)
-    
+    lucas.left(90)
     # Now put the forward and left/right code into a for loop to repeat 4 times.
     # TEST    Did your turtle draw a square?
-        
+    for i in range(4):
+        lucas.forward(100)
+        lucas.left(90)
     # Move your turtle to a new place on the screen using .goto(x, y)
     # x=0 and y=0 is the center of the screen
-    
+    lucas.goto(100, 100)
     # Have your turtle draw a circle using .circle(radius, steps=30)
     # TEST    Did your turtle draw a circle?
-    
+    lucas.begin_fill()
+    lucas.circle(50, 360, 30)
+    lucas.end_fill()
     # Add color to your shape by adding .begin_fill() before drawing the circle
     # and .end_fill() below
 
     # Draw 3 more shapes with different fill colors!
+    lucas.circle(25, 360, 5)
+    lucas.penup()
+    lucas.goto(200, 0)
+    lucas.pendown()
+    lucas.circle(150, 180, 30)
+    lucas.penup()
+    lucas.goto(35, -15)
+    lucas.pendown()
+    lucas.circle(15, 360, 30)
+    lucas.penup()
+    lucas.pendown()
     
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     turtle.done()
--- _01_drawing/_c_logical_turtle.py
+++ _01_drawing/_c_logical_turtle.py
@@ -11,37 +11,37 @@ def screenClicked(x, y):
     print('You pressed: x=' + str(x) + ', y=' + str(y))
     
     # 6. Call the turtle .penup() method
-    
+    lucas.penup()
     # 7. Move the turtle to a new location using .goto(x, y)
-
+    lucas.goto(x, y)
 def turtleClicked(x, y):
     print('turtle clicked!')
     
     # 8. Make a for loop to run the next instructions 3 times
-        
+    for i in range(3):
         # 9. Make the turtle spin 360 degrees using the .right() method
-        
+        lucas.right(360)
         # 10. Use the .color() method and getRandomColor() function to change
         # the color of the turtle,
-        # myTurtle.color(getRandomColor())
-
+        lucas.color(getRandomColor())
 
 if __name__ == '__main__':
     window = turtle.Screen()
     window.setup(width=0.75, height=0.8, startx=0, starty=0)
     
     # 1. Make a new turtle
-    
+    lucas = turtle.Turtle()
     # 2. Make your turtle's shape 'turtle', .shape('turtle')
-    
+    lucas.shape('turtle')
     # 3. Set your turtle's color using .color('green') and .pencolor('blue')
-    
+    lucas.color('green')
+    lucas.pencolor('blue')
     # 4. Set and new width, length, and outline of our turtle
     #    myTurtle.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
-
+    lucas.turtlesize(2, 1, 3)
     # 5. Uncomment the following line and replace 'myTurtle' with your turtle
-    #myTurtle.onclick(turtleClicked)
-
+    lucas.onclick(turtleClicked)
+    
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screenClicked)
     turtle.done()
